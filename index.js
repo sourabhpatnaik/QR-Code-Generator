f7cc321cc8386d03efdf096be4706582bfe20873
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2022-09-15 00:58:08 +0530

    QR code Generator

---
+++ index.js
@@ -0,0 +1,19 @@
+let qr_code = document.querySelector("#qr-code");
+let code_img = document.querySelector("#code-img");
+let loader = document.querySelector("#loading");
+
+qr_code.addEventListener("click", ()=>{
+    loader.style.display ="block";
+
+    code_img.onload = function(){
+        loader.style.display = "none";
+    }
+    code_img.style.display = "block";
+
+
+let input = document.querySelector("#input").value;
+let api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`;
+
+code_img.src = api;
+
+})
